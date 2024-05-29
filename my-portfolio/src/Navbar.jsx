6e7931dada4c3e66b28ea71bc12494f30694da19
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-05-29 19:35:34 -0400

    feat: add state to navbar button

--- my-portfolio/src/Navbar.jsx
+++ my-portfolio/src/Navbar.jsx
@@ -1,4 +1,5 @@
 import './navbar.css'
+import { useState } from 'react';
 const Navbar = () => {
     const handleClick = () => {
         let display = document.getElementById("dropdown-menu").style.display;
@@ -8,23 +9,30 @@ const Navbar = () => {
         else {
             document.getElementById("dropdown-menu").style.display = "none";
         }
+        if (menu == "MENU"){
+            setMenu("CLOSE");
+        }
+        else {
+            setMenu("MENU");
+        }
     }
+    const [menu, setMenu] = useState("MENU");
     return (
         <>
-            <nav className="navbar">
-                <button className="navbar-button" onClick={handleClick} type="button">MENU</button>
-                <button className="navbar-button" type="button">HIRE ME</button>
-            </nav>
-            <div id="dropdown-menu" className="dropdown-menu">
-                <ul>
-                    <li>
-                        <a href="#home">Home</a>
-                    </li>
-                    <li>
-                        <a href="#graphic design">Graphic Design</a>
-                    </li>
-                </ul>
-            </div>
+                <nav className="navbar">
+                    <button className="navbar-button" onClick={handleClick} type="button">{menu}</button>
+                    <button className="navbar-button" type="button">HIRE ME</button>
+                </nav>
+                <div id="dropdown-menu" className="dropdown-menu">
+                    <ul className="list">
+                        <li>
+                            <a href="#home">Home</a>
+                        </li>
+                        <li>
+                            <a href="#graphic design">Graphic Design</a>
+                        </li>
+                    </ul>
+                </div>
         </>
     );
 }
