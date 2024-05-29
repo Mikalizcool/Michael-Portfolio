078ac2db835ff00b3dcaa0933366db06bb851c64
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-05-29 13:04:32 -0400

    feat: add game gif

--- my-portfolio/src/Projects.jsx
+++ my-portfolio/src/Projects.jsx
@@ -1,11 +1,17 @@
 import './index.css'
 import './projects.css'
+import game from './assets/game.gif'
 const Projects = () => {
     return (
         <>
-            <div className="projects-container">
-                <h1 className="h1">I love creating <div className="bold">projects.</div></h1>
-                <p>As part of my web development and programming journey, I've integrated my artistic skills into several fun projects. I have a lot to learn and its been a great journey so far!</p>
+            <div className="project-container">
+                <div>
+                    <h1 className="h1">I love creating <div className="bold">projects.</div></h1>
+                    <p>As part of my web development and programming journey, I've integrated my artistic skills into several fun projects. I have a lot to learn and its been a great journey so far!</p>
+                </div>
+                <div className="projects-container">
+                    <img src={game}></img>
+                </div>
             </div>
         </>
     );
