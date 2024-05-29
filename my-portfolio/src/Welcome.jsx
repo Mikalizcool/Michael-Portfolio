0ea00e70e5a4dd30a417341188db10c9b049b191
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-05-29 10:45:47 -0400

    style: add basic style to welcome component

--- my-portfolio/src/Welcome.jsx
+++ my-portfolio/src/Welcome.jsx
@@ -1,10 +1,19 @@
 import './index.css'
+import welcomeImage from './assets/welcome.gif'
 const Welcome = () => {
     return (
         <>
-            <h1 className="welcome">
-                Hi! My <br /> name is Michael.
-            </h1>
+            <div className="header">
+                <div className="welcome-message">
+                    <h1 className="welcome">
+                        Hi! My <br /> name is <div className="bold-message">Michael.</div>
+                    </h1>
+                    <p className="welcome-description">
+                        I'm a <div className="bold-description">web developer</div> and <div className="bold-description">graphic designer</div> from New York.
+                    </p>
+                </div>
+                <img src= {welcomeImage}></img>
+            </div>
         </>
     );
 }
