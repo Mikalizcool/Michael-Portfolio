a925e74419c7fd7c96c1b1af326730cb76c2d831
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-05-29 17:43:10 -0400

    feat: add more projects

--- my-portfolio/src/Projects.jsx
+++ my-portfolio/src/Projects.jsx
@@ -20,7 +20,7 @@ const Projects = () => {
                         <div className="project-description">
                             <h1>2d Beat em' up Game</h1>
                             <p>An in progress video game created in Unity using the c# programming language.</p>
-                            <p>It has a comedic halloween theme and I plan on releasing an early access version in Fall.</p>
+                            <p>This game features the ability to change the player's state dynamically, creating an exciting gameplay experience.</p>
                         </div>
                     </div>
                     <hr className="horizontal-line"></hr>
@@ -28,10 +28,40 @@ const Projects = () => {
                         <div className="project-description">
                             <h1>Memory Game</h1>
                             <p>A memory game created using React.</p>
-                            <p>It has a comedic halloween theme and I plan on releasing an early access version in Fall.</p>
+                            <p>This project focuses on utilizing state while fetching and using data from an external API.</p>
                         </div>
                         <div className="space"></div>
                         <img onClick= {handleClick} src={matchingGame}></img>
+                    </div>
+                    <hr className="horizontal-line"></hr>
+                    <div className="project">
+                    <img onClick= {handleClick} src={matchingGame}></img>
+                    <div className="space"></div>
+                        <div className="project-description">
+                            <h1>Companion App</h1>
+                            <p>A companion app for the Mass Effect game series made using React.</p>
+                            <p>This project focuses on providing information on the Mass Effect game series in a way that is interesting for fans and newcomers alike.</p>
+                        </div>
+                    </div>
+                    <hr className="horizontal-line"></hr>
+                    <div className="project">
+                        <div className="project-description">
+                            <h1>CV-Application</h1>
+                            <p>A job application made using React.</p>
+                            <p>This project uses user input and state to update a form.</p>
+                        </div>
+                        <div className="space"></div>
+                        <img onClick= {handleClick} src={matchingGame}></img>
+                    </div>
+                    <hr className="horizontal-line"></hr>
+                    <div className="project">
+                    <img onClick= {handleClick} src={matchingGame}></img>
+                    <div className="space"></div>
+                        <div className="project-description">
+                            <h1>Defeat the hostile A.I!</h1>
+                            <p>Can you defeat this A.I in a game of rock paper scissors?</p>
+                            <p>This project involves making a simple game more interesting with animations.</p>
+                        </div>
                     </div>
                 </div>
             </div>
