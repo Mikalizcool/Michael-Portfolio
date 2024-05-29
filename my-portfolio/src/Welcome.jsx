52b607e17719001701fccd22cdf095a0cc866c00
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-05-29 09:46:50 -0400

    feat: create welcome component

---
+++ my-portfolio/src/Welcome.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+const Welcome = () => {
+    return (
+        <>
+            <h1 className="welcome">
+                Hi! My <br /> name is Michael.
+            </h1>
+        </>
+    );
+}
+ 
+export default Welcome;
