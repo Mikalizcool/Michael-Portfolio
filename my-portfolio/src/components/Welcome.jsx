57fafabba39f471b76e9d01316bbf8b74aafaf2a
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-07-18 11:17:17 -0400

    fix: make div not decendent of p

--- my-portfolio/src/components/Welcome.jsx
+++ my-portfolio/src/components/Welcome.jsx
@@ -10,9 +10,9 @@ const Welcome = () => {
                         <h1 className="welcome">
                             Hi! My <br /> name is <div className="bold-message">Michael.</div>
                         </h1>
-                        <p className="welcome-description">
+                        <div className="welcome-description">
                             I'm a <div className="bold-description">web developer</div> and <div className="bold-description">graphic designer</div> from New York.
-                        </p>
+                        </div>
                     </div>
                     <img src= {welcomeImage}></img>
                 </div>
