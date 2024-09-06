924014389fd5a9ee4be6e0f579ea39e3e0f4ff0d
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-09-06 11:06:15 -0400

    feat: add graphic design work

--- my-portfolio/src/pages/Graphicdesign.jsx
+++ my-portfolio/src/pages/Graphicdesign.jsx
@@ -9,6 +9,7 @@ import review from '../assets/year-in-review.png'
 import technical from '../assets/technical-art.png'
 import postera from '../assets/poster-a.png'
 import bkcover from '../assets/book_cover.png'
+import seal from '../assets/seal.jpg'
 const Graphicdesign = () => {
     const handleClick = (name) => {
         if (name=="report"){
@@ -76,66 +77,76 @@ const Graphicdesign = () => {
 
                         <div className="work-container">
                             <div className="work-left">
-                                <img src={report}></img>
+                                <img src={seal}></img>
                             </div>
                             <div className="work-right">
-                                <h1 className="work-title">Annual Report</h1>
-                                <p className="work-description"> The annual report for DHS HSIN. Click to view full version hosted on the DHS site.
+                                <h1 className="work-title">IMDE Seal</h1>
+                                <p className="work-description"> The seal I created for the IMDE program of the DHS.
                                 </p>
-                                <p>Created using Adobe Indesign, Adobe Photoshop and Adobe Illustrator.</p>
-                                <button className="work-button" onClick={() => (handleClick("report"))}>Check it out</button>
+                                <p>Created using Adobe Illustrator.</p>
                             </div>
                         </div>
 
                         <div className="work-container">
                             <div className="work-left">
+                            <h1 className="work-title">Annual Report</h1>
+                            <p className="work-description"> The annual report for DHS HSIN. Click to view full version hosted on the DHS site.
+                            </p>
+                            <p>Created using Adobe Indesign, Adobe Photoshop and Adobe Illustrator.</p>
+                            <button className="work-button" onClick={() => (handleClick("report"))}>Check it out</button>
+                            </div>
+                            <div className="work-right">
+                                <img src={report}></img>
+                            </div>
+                        </div>
+
+                        <div className="work-container">
+                            <div className="work-left">
+                            <img src={review}></img>
+                            </div>
+                            <div className="work-right">
                                 <h1 className="work-title">Year in Review</h1>
                                 <p className="work-description"> The Year in Review for DHS HSIN. Click to view full version hosted on the DHS site.
                                 </p>
                                 <p>Created using Adobe Indesign and Adobe Illustrator.</p>
                                 <button className="work-button" onClick={() => (handleClick("review"))}>Check it out</button>
-
-                            </div>
-                            <div className="work-right">
-                                <img src={review}></img>
                             </div>
                         </div>
 
                         <div className="work-container">
                             <div className="work-left">
-                                <img src={technical}></img>
-                            </div>
-                            <div className="work-right">
                                 <h1 className="work-title">Technical Illustration</h1>
                                 <p className="work-description"> An example of my ability to create technical illustrations.
                                 </p>
                                 <p>Created using Clip Studio Paint.</p>
                             </div>
+                            <div className="work-right">
+                                <img src={technical}></img>
+                            </div>
                         </div>
 
                         <div className="work-container">
                             <div className="work-left">
+                                <img src={postera}></img>
+                            </div>
+                            <div className="work-right">
                                 <h1 className="work-title">Poster Commission</h1>
                                 <p className="work-description"> A poster I created for the popular web show The Angry Joe Show.
                                 </p>
                                 <p>Created using Adobe Photoshop.</p>
                             </div>
-                            <div className="work-right">
-                                <img src={postera}></img>
-                            </div>
                         </div>
 
                         <div className="work-container">
                             <div className="work-left">
-                                <img src={bkcover}></img>
-                            </div>
-                            <div className="work-right">
                                 <h1 className="work-title">Book cover</h1>
                                 <p className="work-description"> The cover for the book I wrote. This complicated project helped me learn project management skills.
                                 </p>
                                 <p>Created using Clip Studio Paint.</p>
                                 <button className="work-button" onClick={() => (handleClick("bkcover"))}>Check it out</button>
-
+                            </div>
+                            <div className="work-right">
+                                <img src={bkcover}></img>
                             </div>
                         </div>
                     </div>
