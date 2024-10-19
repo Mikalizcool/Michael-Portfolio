61531bc3f84c51164adfb39331e514213cf08d6f
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-10-18 23:21:20 -0400

    refactor: change projects page title

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -28,7 +28,7 @@ const Projects = () => {
         <>
             <div className="project-container">
                 <div>
-                    <h1 className="h1">I love creating <div className="bold">projects.</div></h1>
+                    <h1 className="h1">I love <div className="bold">creating.</div></h1>
                     <p>As part of my web development and programming journey, I've integrated my artistic skills into several fun projects. I have a lot to learn and its been a great journey so far!</p>
                 </div>
                 <div className="projects-container">
