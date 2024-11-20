2e6e22d90d14320964516504d4c95abaadf2068c
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-11-20 18:10:39 -0500

    style: change font color

--- my-portfolio/src/components/Welcome.jsx
+++ my-portfolio/src/components/Welcome.jsx
@@ -8,7 +8,7 @@ const Welcome = () => {
                 <div className="starter">
                     <div className="welcome-message">
                         <h1 className="welcome">
-                            Hi! My <br /> name is <div className="bold-message">Michael.</div>
+                            Hi! My name is Michael.
                         </h1>
                         <div className="welcome-description">
                             I'm a <div className="bold-description">web developer</div> and <div className="bold-description">graphic designer</div> from New York.
