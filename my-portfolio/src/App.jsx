d4fd8e6d7a84ef1544e6baa349618e10570011f1
Michael <78291922+Mikalizcool@users.noreply.github.com>
2025-04-05 15:58:15 -0400

    fix: import index.css

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from 'react'
 import './App.css'
+import './index.css'
 import Welcome from './components/Welcome'
 import Body from './components/Body'
 import Projects from './components/Projects'
